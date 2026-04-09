6530c6ac22f40f5393022c0b17ff2ce71e6134e9
trincis <katrinagraudina1@gmail.com>
2026-04-09 17:19:24 +0300

    initscr() un saku domat par speli

--- LU_Bomberman.c
+++ LU_Bomberman.c
@@ -13,17 +13,30 @@ Programma izveidota: ??.04.2026
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+/**
+Kartes simboli:
+H - ciets
+S - miksts
+1...8 speletajs (moz krasainus?)
+B - bumba
+R - radiusa palielinasana
+T - bumbas atskaites laika palielinasana
+**/
 
-void LaukumaIzveide(FILE conf){
+//funkcijai jaizveido laukums un jasagatavo speles sakums
+void start(FILE conf){
+///apstrada konfiguracijas faila doto informaciju
+///<augst> <plat> <atrums> <boom ilgums> <boom attalums> <boom atskaite>
+///laukuma karte
 
 }
 
 int main(){
     FILE conf = fopen("config", "r");
 
-    LaukumaIzveide(conf);
+    initscr();
 
-
+    start(conf);
 
     return 0;
 }
