f128ebb61ee9bc20f51939cbd2582d78398a4e31
trincis <katrinagraudina1@gmail.com>
2026-04-09 17:02:13 +0300

    initial commit

---
+++ LU_Bomberman.c
@@ -0,0 +1,29 @@
+/**
+LU_Bomberman.c
+
+Programma ir spēle balstīta uz retro spēli Bomberman
+
+Spēles laukuma konfigurācija - fails "config"
+
+Autori: Barbara Terēze Graudiņa, bg24008
+        Katrīna Anna Graudiņa, kg21076
+
+Programma izveidota: ??.04.2026
+**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+void LaukumaIzveide(FILE conf){
+
+}
+
+int main(){
+    FILE conf = fopen("config", "r");
+
+    LaukumaIzveide(conf);
+
+
+
+    return 0;
+}
